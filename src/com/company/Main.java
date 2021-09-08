cd1952b7b75d295a1b374a708bcfcd50d5cfc758
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-09-08 11:52:35 +0530

    UC3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,24 +5,28 @@ public class Main {
     public static void main(String[] args) {
         //constants
         int is_full_time = 1;
+        int is_part_time=2;
         int is_absent = 0;
-        int emp_hr = 8;
+        int emp_hr = 16;
         int Wage_per_hr = 20;
 
         //computation
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == 1) {
             System.out.println("EMPLOYEE IS FULL_TIME");
-            System.out.println("Emp hour");
-            System.out.println(emp_hr = 8);
-        } else {
+            System.out.println("Emp hour");System.out.println(emp_hr=16);
+        }else if(empCheck==2){
+            System.out.println("EMPLOYEE IS PART_TIME");
+            System.out.println("Emp hour");System.out.println(emp_hr=8);
+
+        }
+        else{
             System.out.println("EMPLOYEE IS ABSENT");
-            System.out.println("Emp hour");
-            System.out.println(emp_hr = 0);
+            System.out.println("Emp hour");System.out.println(emp_hr = 0);
         }
 
         int dailywage = (emp_hr * Wage_per_hr);
-        //printing Emp Daily Wage
+        //printing the Emp Daily Wage
         System.out.println("Emp Daily Wage");
         System.out.println(dailywage);
     }
