934c6a80396b66ba58e982e1db6ec016b329ea15
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-09-08 13:21:14 +0530

    UC6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,35 @@
 package com.company;
 
 public class Main {
-    public static final int IS_PRT_TIME = 1;
+    public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 2;
-    private static int totalEmpwage;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH =100;
+
+
 
     public static void main(String[] args) {
         // Variables
-        int empHrs = 0; int empWage = 0; totalEmpwage = 0;
+        int empHrs = 0; int empWage = 0;
+        int totalWorkingDays = 0;
         //computation
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-            switch (empCheck) {
-                case IS_PRT_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-
-            }
-            empWage = empHrs * EMP_RATE_HOUR;
-            totalEmpwage += empWage;
-            System.out.println("Emp Wage:" + empWage);
-
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        int empHrs = 0;
+        totalWorkingDays++;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
         }
-        System.out.println("total Emp Wage: " + totalEmpwage);
+        totalEmpHrs += empHrs;
+        int empWage = empHrs * EMP_RATE_HOUR;
+        totalEmpwage += empWage;
     }
 
-}
