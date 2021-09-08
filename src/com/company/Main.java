2f623eac866728026a69370222a54268eaa74785
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-09-08 11:44:32 +0530

    UC2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //constant
+        //constants
         int is_full_time = 1;
+        int is_absent = 0;
+        int emp_hr = 8;
+        int Wage_per_hr = 20;
 
         //computation
         double empCheck = Math.floor(Math.random() * 10) % 2;
-
         if (empCheck == 1) {
-            System.out.println("EMPLOYEE IS PRESENT");
+            System.out.println("EMPLOYEE IS FULL_TIME");
+            System.out.println("Emp hour");
+            System.out.println(emp_hr = 8);
         } else {
             System.out.println("EMPLOYEE IS ABSENT");
+            System.out.println("Emp hour");
+            System.out.println(emp_hr = 0);
         }
+
+        int dailywage = (emp_hr * Wage_per_hr);
+        //printing Emp Daily Wage
+        System.out.println("Emp Daily Wage");
+        System.out.println(dailywage);
     }
 }
