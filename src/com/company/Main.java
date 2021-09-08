98262b569b177d71809d653946c668738c89110b
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-09-08 11:30:28 +0530

    UC1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage computation program ");
-	// write your code here
+        //constant
+        int is_full_time = 1;
+
+        //computation
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        if (empCheck == 1) {
+            System.out.println("EMPLOYEE IS PRESENT");
+        } else {
+            System.out.println("EMPLOYEE IS ABSENT");
+        }
     }
 }
