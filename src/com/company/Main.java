8eb181548b28ec91d69d11b650b3fbba2c7d7ada
s-h-u-b-s07 <shubhamgawali.sag@gmail.com>
2021-09-08 12:45:10 +0530

    UC5

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,35 @@
 package com.company;
 
 public class Main {
-    //constants
-    public static final int IS_PART_TIME = 1;
+    public static final int IS_PRT_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int EMP_RATE_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 2;
+    private static int totalEmpwage;
 
     public static void main(String[] args) {
-        //variables
-        int empHrs = 0;
-        int empWage = 0;
+        // Variables
+        int empHrs = 0; int empWage = 0; totalEmpwage = 0;
         //computation
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
-            case IS_FULL_TIME:
-                empHrs = 8;
-                break;
-            case IS_PART_TIME:
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+            switch (empCheck) {
+                case IS_PRT_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+
+            }
+            empWage = empHrs * EMP_RATE_HOUR;
+            totalEmpwage += empWage;
+            System.out.println("Emp Wage:" + empWage);
+
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Emp Wage: " + empWage);
+        System.out.println("total Emp Wage: " + totalEmpwage);
     }
+
 }
